9ea5bbc4f130206105062c93bb77998c8cd17097
Egor Spirin <eg.spirin@gmail.com>
2020-09-21 20:43:40 +0200

    Added descriptions of constants in config

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 # Values in meters
 
-LANE_OFFSET = 2
-LANE_OFFSET_LENGTH_REDUCTION = 0.5
-INTERSECTION_OFFSET = 3
-NODES_POSITION_ERROR = 1
-MIN_PAIR_LENGTH = 2
+LANE_OFFSET = 2  # Distance to which each lane is going to be moved
+LANE_OFFSET_LENGTH_REDUCTION = 0.5  # Distance reduction of each offsetted segment.
+# The further the lane is - the shorter it is
+
+INTERSECTION_OFFSET = 3  # Shorten segment which neighbors an intersection by this length
+NODES_POSITION_ERROR = 1  # If nodes are closer to each other than NODES_POSITION_ERROR, then they are the same node
+MIN_PAIR_LENGTH = 2  # Filter out pairs shorter than MIN_PAIR_LENGTH
